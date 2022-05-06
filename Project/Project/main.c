6159d363308ff745719d1045448d5d913191adb8
chamaben <chamabenlafkih.cb@gmail.com>
2022-05-06 21:38:36 +0900

    start

---
+++ Project/Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project.c
+ *
+ * Created: 06/05/2022 21:26:39
+ * Author : pc_benlafkih
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
